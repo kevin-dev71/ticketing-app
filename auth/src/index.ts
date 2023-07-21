261108d8c3f4f592ed1e13113dd7a334d1dcc0ee
Kevin Dev <kevin.developer71@gmail.com>
2023-07-20 20:44:14 -0600

    feat: user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,40 @@
-import express from "express"
-import {json} from "body-parser"
-import { currentUserRouter } from "./routes/current-user"
-import { signinRouter } from "./routes/signin"
-import { signupRouter } from "./routes/signup"
-import { signoutRouter } from "./routes/signout"
-import { errorHandler } from "./middlewares/error-handler"
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
 
-const app = express()
-app.use(json())
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signupRouter)
-app.use(signoutRouter)
+const app = express();
+app.use(json());
 
-app.use(errorHandler)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 app.listen(3000, () => {
-  console.log("Server started on port 3000!")
-})
+  console.log("Server started on port 3000!");
+});
+
+start()
