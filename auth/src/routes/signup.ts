8b020466ec1cf4d5508ade2d5bb0cb6a80e9a1d0
Kevin Dev <kevin.developer71@gmail.com>
2023-07-29 00:12:24 -0600

    feat: jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -4,6 +4,7 @@ import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -34,6 +35,17 @@ router.post(
 
     const user = User.build({email, password})
     await user.save()
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, "SECRET")
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    }
 
     res.status(201).send(user);
   }
